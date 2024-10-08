213357be09ca0896a5b1a15712dcd509dce3626e
Kevin Ye <chunye@live.com>
2024-10-08 22:17:39 +0800

    add vpc

--- main.tf
+++ main.tf
@@ -18,3 +18,21 @@ resource "aws_instance" "web" {
     Name = random_pet.name.id
   }
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
