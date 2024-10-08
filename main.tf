83c7ed752e5bf95bb78d68b8658893c5a4d803a9
Kevin Ye <chunye@live.com>
2024-10-08 23:17:18 +0800

    fix vpc az issue

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "vpc" {
   name    = "my-vpc"
   cidr    = "10.0.0.0/16"
 
-  azs             = ["ap-southeast-1-1a"]
+  azs             = ["ap-southeast-1a"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
